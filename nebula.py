d434a433a469967186be20497ad2d73342ecd4cb
WangXiaoDong <wangxiaodong@ebrun.com>
2021-01-15 20:08:17 +0800

    页面模板提交

--- nebula.py
+++ nebula.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('nebula.html')
+    return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('nebula_login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
